8e6c991bce0bd9566503e946ed8b4a1013b750bd
0585016892 <tranhung6829@gmail.com>
2025-09-18 10:47:49 +0700

    deploy khách hàng

--- src/api/config.js
+++ src/api/config.js
@@ -1,13 +1,31 @@
-// src/api/config.js
+// // src/api/config.js
 
+// import axios from "axios";
+
+
+// const API_URL = process.env.REACT_APP_API_URL;
+// console.log(API_URL)
+
+// axios.get(`${process.env.REACT_APP_API_URL}/products`)
+//   .then(res => console.log(res.data))
+//   .catch(err => console.error(err));
+
+// export default API_URL;
+// src/api/config.js
 import axios from "axios";
 
-
 const API_URL = process.env.REACT_APP_API_URL;
-console.log(API_URL)
 
-axios.get(`${process.env.REACT_APP_API_URL}/products`)
-  .then(res => console.log(res.data))
-  .catch(err => console.error(err));
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "ngrok-skip-browser-warning": "true", // để bỏ warning page
+  },
+});
 
-export default API_URL;
+// Test thử (sau có thể xóa đi, chỉ để check ban đầu)
+api.get("/products")
+  .then((res) => console.log("API data:", res.data))
+  .catch((err) => console.error("API error:", err));
+
+export default api;
