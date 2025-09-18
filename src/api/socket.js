a1924295c69abb98d49b4cd22dbf00e4d36c7a2d
0585016892 <tranhung6829@gmail.com>
2025-09-18 11:14:05 +0700

    deploy khách hàng

--- src/api/socket.js
+++ src/api/socket.js
@@ -1,5 +1,9 @@
 import { io } from "socket.io-client";
 
 export const socket = io(process.env.REACT_APP_WEB_URL, {
+  withCredentials: true,
+  extraHeaders: {
+    "ngrok-skip-browser-warning": "true", // tránh warning
+  },
   transports: ["websocket"], // đảm bảo không bị polling
 });
